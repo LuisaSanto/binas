0bf3c67b496bf1500ee378f2223ecd2e1f652f8a
Kevin <kevindevos9542@gmail.com>
2018-03-24 21:26:21 +0000

    add basic Binas structure + activateUser operation

---
+++ binas-ws/src/main/java/org/binas/ws/BinasEndpointManager.java
@@ -0,0 +1,4 @@
+package org.binas.ws;
+// TODO
+public class BinasEndpointManager {
+}
---
+++ binas-ws/src/main/java/org/binas/ws/BinasPortImpl.java
@@ -0,0 +1,67 @@
+package org.binas.ws;
+
+import org.binas.domain.Binas;
+import org.binas.exception.EmailExistsException;
+import org.binas.exception.InvalidEmailException;
+
+import java.util.List;
+
+public class BinasPortImpl implements BinasPortType{
+
+    @Override
+    public List<StationView> listStations(Integer numberOfStations, CoordinatesView coordinates){
+        return null;
+    }
+
+    @Override
+    public StationView getInfoStation(String stationId) throws InvalidStation_Exception{
+        return null;
+    }
+
+    @Override
+    public int getCredit(String email) throws UserNotExists_Exception{
+        return 0;
+    }
+
+    @Override
+    public UserView activateUser(String email) throws EmailExists_Exception, InvalidEmail_Exception{
+        try{
+            return Binas.getInstance().activateUser(email);
+        }catch(EmailExistsException e){
+            throw new EmailExists_Exception("Email Exists", new EmailExists());
+        }catch(InvalidEmailException e){
+            throw new InvalidEmail_Exception("Invalid Email", new InvalidEmail());
+        }
+    }
+
+
+    @Override
+    public void rentBina(String stationId, String email) throws AlreadyHasBina_Exception, InvalidStation_Exception, NoBinaAvail_Exception, NoCredit_Exception, UserNotExists_Exception{
+
+    }
+
+    @Override
+    public void returnBina(String stationId, String email) throws FullStation_Exception, InvalidStation_Exception, NoBinaRented_Exception, UserNotExists_Exception{
+
+    }
+
+    @Override
+    public String testPing(String inputMessage){
+        return null;
+    }
+
+    @Override
+    public void testClear(){
+
+    }
+
+    @Override
+    public void testInitStation(String stationId, int x, int y, int capacity, int returnPrize) throws BadInit_Exception{
+
+    }
+
+    @Override
+    public void testInit(int userInitialPoints) throws BadInit_Exception{
+
+    }
+}
