725f923956b17fbf2338abc16245e6c9ae9b1bd6
Kevin <kevindevos9542@gmail.com>
2018-03-24 22:08:30 +0000

    add @WebService annotation to BinasPortImpl

--- binas-ws/src/main/java/org/binas/ws/BinasPortImpl.java
+++ binas-ws/src/main/java/org/binas/ws/BinasPortImpl.java
@@ -4,8 +4,17 @@ import org.binas.domain.Binas;
 import org.binas.exception.EmailExistsException;
 import org.binas.exception.InvalidEmailException;
 
+import javax.jws.WebService;
 import java.util.List;
 
+@WebService(
+        endpointInterface = "org.binas.ws.BinasPortType",
+        wsdlLocation = "binas.wsdl",
+        name = "BinasWebService",
+        portName = "BinasPort",
+        targetNamespace = "http://ws.binas.binas.org/",
+        serviceName = "BinasService"
+)
 public class BinasPortImpl implements BinasPortType{
 
     @Override
