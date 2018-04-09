3ae7ac2cdb2eaabbfbb90d1f88cd515db39c6449
Luisa Santo <luisa.santo@gmail.com>
2018-04-09 11:06:59 +0100

    Improve BinasManager

--- binas-ws/src/main/java/org/binas/domain/BinasManager.java
+++ binas-ws/src/main/java/org/binas/domain/BinasManager.java
@@ -16,59 +16,40 @@ import java.util.Vector;
 /* Gestor do binas, possui funcionalidades de administracao */
 public class BinasManager {
 
-	// Singleton -------------------------------------------------------------
+    // Singleton -------------------------------------------------------------
 
-	private BinasManager() {
-	}
+    private BinasManager() {
+    }
 
-	/**
-	 * SingletonHolder is loaded on the first execution of Singleton.getInstance()
-	 * or the first access to SingletonHolder.INSTANCE, not before.
-	 */
-	private static class SingletonHolder {
-		private static final BinasManager INSTANCE = new BinasManager();
-	}
+    /**
+     * SingletonHolder is loaded on the first execution of Singleton.getInstance()
+     * or the first access to SingletonHolder.INSTANCE, not before.
+     */
+    private static class SingletonHolder {
+        private static final BinasManager INSTANCE = new BinasManager();
+    }
 
-	public static synchronized BinasManager getInstance() {
-		return SingletonHolder.INSTANCE;
-	}
+    public static synchronized BinasManager getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
 
-	public static synchronized List<StationView> getStations(String uddiUrl){
-        try{
+    public static synchronized List<StationView> getStations(String uddiUrl) {
+        try {
             UDDINaming uddiNaming = new UDDINaming(uddiUrl);
 
             // get all station records in UDDI , using * wildcard
             Collection<UDDIRecord> UDDIrecords = uddiNaming.listRecords("A58_Station*");
 
             Vector<String> urls = new Vector<>();
-            for(UDDIRecord uddiRecord : UDDIrecords){
+            for (UDDIRecord uddiRecord : UDDIrecords) {
                 urls.add(uddiRecord.getUrl());
             }
 
             Vector<StationView> allStationViews = new Vector<>();
             StationClient stationClient = null;
-            for(int i = 0; i < urls.size(); i++){
+            for (int i = 0; i < urls.size(); i++) {
                 stationClient = new StationClient(urls.elementAt(i));
-                StationView temp = new StationView();
-
-                String id = stationClient.getInfo().getId();
-                Integer x = stationClient.getInfo().getCoordinate().getX();
-                Integer y = stationClient.getInfo().getCoordinate().getY();
-                int capacity = stationClient.getInfo().getCapacity();
-                int totalGets = stationClient.getInfo().getTotalGets();
-                int totalReturns = stationClient.getInfo().getTotalReturns();
-                int availableBinas = stationClient.getInfo().getAvailableBinas();
-                int freeDocks = stationClient.getInfo().getFreeDocks();
-                temp.setId(id);
-                temp.getCoordinate().setX(x);
-                temp.getCoordinate().setY(y);
-                temp.setCapacity(capacity);
-                temp.setTotalGets(totalGets);
-                temp.setTotalReturns(totalReturns);
-                temp.setAvailableBinas(availableBinas);
-                temp.setFreeDocks(freeDocks);
-
-
+                StationView temp = convertStations(stationClient);
                 allStationViews.addElement(temp);
             }
             return allStationViews;
@@ -80,15 +61,68 @@ public class BinasManager {
         return null;
     }
 
-    public static synchronized StationView getStation(String stationId, String uddiUrl) {
+    public static synchronized StationView getStationView(String stationId, String uddiUrl) {
         Vector<StationView> allStationViews = (Vector<StationView>) getStations(uddiUrl);
-        for(StationView station : allStationViews){
-            if(station.getId().equals(stationId)){
+        for (StationView station : allStationViews) {
+            if (station.getId().equals(stationId)) {
                 return station;
             }
         }
         return null;
     }
+
+
+    public static synchronized StationView convertStations(StationClient stationClient) {
+        StationView temp = new StationView();
+
+        String id = stationClient.getInfo().getId();
+        Integer x = stationClient.getInfo().getCoordinate().getX();
+        Integer y = stationClient.getInfo().getCoordinate().getY();
+        int capacity = stationClient.getInfo().getCapacity();
+        int totalGets = stationClient.getInfo().getTotalGets();
+        int totalReturns = stationClient.getInfo().getTotalReturns();
+        int availableBinas = stationClient.getInfo().getAvailableBinas();
+        int freeDocks = stationClient.getInfo().getFreeDocks();
+        temp.setId(id);
+        temp.getCoordinate().setX(x);
+        temp.getCoordinate().setY(y);
+        temp.setCapacity(capacity);
+        temp.setTotalGets(totalGets);
+        temp.setTotalReturns(totalReturns);
+        temp.setAvailableBinas(availableBinas);
+        temp.setFreeDocks(freeDocks);
+
+        return temp;
+    }
+
+    public static synchronized StationClient getStationClient(String stationID, String uddiUrl) {
+        try {
+            UDDINaming uddiNaming = new UDDINaming(uddiUrl);
+
+            // get all station records in UDDI , using * wildcard
+            Collection<UDDIRecord> UDDIrecords = uddiNaming.listRecords("A58_Station*");
+
+            Vector<String> urls = new Vector<>();
+            for (UDDIRecord uddiRecord : UDDIrecords) {
+                urls.add(uddiRecord.getUrl());
+            }
+
+            StationClient stationClient = null;
+            for (int i = 0; i < urls.size(); i++) {
+                stationClient = new StationClient(urls.elementAt(i));
+                StationView temp = convertStations(stationClient);
+                if (temp.getId().equals(stationID)) {
+                    return stationClient;
+                }
+            }
+        } catch (UDDINamingException e) {
+            System.out.println("Problem reaching UDDI from Binas");
+        } catch (StationClientException e) {
+            System.out.println("Problem creating StationClient");
+        }
+        return null;
+    }
+
 	
 	// TODO
 
