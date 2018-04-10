087fcdc72157bc2e95d7c5cccbc4b7a4fb0e5e63
Luisa Santo <luisa.santo@gmail.com>
2018-04-10 11:56:57 +0100

    Update RentBinaMethodIT test

--- binas-ws-cli/src/test/java/org/binas/ws/it/RentBinaMethodIT.java
+++ binas-ws-cli/src/test/java/org/binas/ws/it/RentBinaMethodIT.java
@@ -8,26 +8,32 @@ import org.junit.Test;
 import static junit.framework.TestCase.fail;
 
 public class RentBinaMethodIT extends BaseIT {
+    private String validEmail1 = "valid.email@valid.domain";
+    private String validEmail2 = "valid1.email@valid.domain";
+    private String stationID = "A58_Station1";
+    private String noStationID = "station ID";
     UserView user;
     StationView station;
-    String validEmail1 = "valid.email@valid.domain";
-    String validEmail2 = "valid1.email@valid.domain";
-    String stationID = "station ID";
-    String noStationID = "station ID 2";
+
+
     @Before
     public void setUp() {
-        client.testClear();
+        //client.testClear();
         try {
             user = client.activateUser(validEmail1);
+            station = client.getInfoStation(stationID);
+            client.testInitStation(stationID, 1, 1, 20, 2);
+            System.out.println(station.getId());
             user.setCredit(10);
             user.setHasBina(false);
-            user.setEmail(validEmail1);
-            station.setAvailableBinas(10);
-            station.setId(stationID);
-        } catch (EmailExists_Exception | InvalidEmail_Exception e) {
-            //TODO SOMETHING HERE
-        }
 
+
+        } catch (EmailExists_Exception | InvalidEmail_Exception | InvalidStation_Exception e) {
+            //TODO SOMETHING HERE
+            System.out.println("yay error");
+        } catch (BadInit_Exception e) {
+            e.printStackTrace();
+        }
     }
 
     @Test(expected = InvalidStation_Exception.class)
@@ -65,11 +71,10 @@ public class RentBinaMethodIT extends BaseIT {
 
     @Test(expected = NoBinaAvail_Exception.class)
     public void noBinaAvail() throws UserNotExists_Exception, InvalidStation_Exception,
-            AlreadyHasBina_Exception, NoBinaAvail_Exception, NoCredit_Exception {
+            AlreadyHasBina_Exception, NoBinaAvail_Exception, NoCredit_Exception, BadInit_Exception {
         station.setAvailableBinas(0);
         client.rentBina(stationID, validEmail1);
     }
-
 
     @After
     public void tearDown(){
