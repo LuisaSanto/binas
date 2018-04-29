268be081e7ac1d792e0e937d77ee25da512f6e4c
Kevin <kevindevos9542@gmail.com>
2018-04-29 14:12:44 +0100

    StationPortImpl now refers to station.2_0.wsdl instead of station.wsdl

--- station-ws-cli/src/test/java/org/binas/station/ws/it/SetBalanceIT.java
+++ station-ws-cli/src/test/java/org/binas/station/ws/it/SetBalanceIT.java
@@ -1,4 +1,33 @@
 package org.binas.station.ws.it;
 
-public class SetBalanceIT {
+import org.binas.station.ws.TaggedBalance;
+import org.binas.station.ws.UserNotExist_Exception;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static junit.framework.TestCase.fail;
+
+public class SetBalanceIT extends BaseIT{
+    final String userEmail = "someone@email";
+
+    final int value = 2;
+
+    @Test
+    public void success(){
+        TaggedBalance taggedBalance = new TaggedBalance();
+        taggedBalance.setTag(1);
+        taggedBalance.setValue(value);
+
+        String response = client.setBalance(userEmail, taggedBalance);
+        Assert.assertEquals("ack", response);
+
+        try{
+            TaggedBalance responseTaggedBalance = client.getBalance(userEmail);
+            Assert.assertEquals(value, responseTaggedBalance.getValue());
+
+        } catch(UserNotExist_Exception e){
+            fail();
+        }
+    }
+
 }
