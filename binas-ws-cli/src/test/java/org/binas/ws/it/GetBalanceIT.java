a8f071f13820e0daec351cbbec7cd1d2d4033616
Kevin <kevindevos9542@gmail.com>
2018-05-02 20:28:13 +0100

    fix get balance success test

--- binas-ws-cli/src/test/java/org/binas/ws/it/GetBalanceIT.java
+++ binas-ws-cli/src/test/java/org/binas/ws/it/GetBalanceIT.java
@@ -1,17 +1,12 @@
 package org.binas.ws.it;
 
-import org.binas.station.ws.GetBalanceResponse;
-import org.binas.station.ws.SetBalanceResponse;
 import org.binas.station.ws.TaggedBalance;
-import org.binas.station.ws.UserNotExist_Exception;
 import org.binas.ws.UserNotExists_Exception;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import javax.xml.ws.Response;
-import java.util.concurrent.ExecutionException;
 
 import static junit.framework.TestCase.fail;
 
