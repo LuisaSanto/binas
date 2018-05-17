0924539f7416cb2703e27e1e50bd2c2f9b13f437
Kevin <kevindevos9542@gmail.com>
2018-05-17 19:31:15 +0100

    add integrity check tests for mac digest, closes #60

---
+++ binas-ws-cli/src/test/java/org/binas/ws/it/ReplayAttackIT.java
@@ -0,0 +1,15 @@
+package org.binas.ws.it;
+
+import org.junit.Test;
+
+public class ReplayAttackIT extends BaseIT{
+    public final String VALID_SERVER_NAME = "binas@A58.binas.org";
+    public final String VALID_CLIENT_NAME = "alice@A58.binas.org";
+    public final String VALID_CLIENT_PASSWORD = "r6p67xdOV";
+
+    @Test
+    public void noReplaySuccess(){
+
+
+    }
+}
