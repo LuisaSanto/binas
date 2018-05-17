df411793379150d5af2dec2c23769ea0a79e5e78
Luisa Santo <luisa.santo@gmail.com>
2018-05-17 22:31:07 +0100

    Add TimeHandler

--- ws-handlers/src/main/java/example/ws/handler/TimeHandler.java
+++ ws-handlers/src/main/java/example/ws/handler/TimeHandler.java
@@ -1,10 +1,14 @@
 package example.ws.handler;
 
 import javax.xml.namespace.QName;
+import javax.xml.soap.*;
 import javax.xml.ws.handler.MessageContext;
 import javax.xml.ws.handler.soap.SOAPHandler;
 import javax.xml.ws.handler.soap.SOAPMessageContext;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Iterator;
 import java.util.Set;
 
 /**
@@ -25,12 +29,60 @@ public class TimeHandler implements SOAPHandler<SOAPMessageContext> {
 		return (outbound) ? handleOutbound(smc) : handleInbound(smc);
 	}
 
+	private SOAPHeader getHeader(SOAPEnvelope env) throws SOAPException {
+		SOAPHeader header = env.getHeader();
+		if (header == null)
+			header = env.addHeader();
+		return header;
+	}
+
 	private boolean handleInbound(SOAPMessageContext smc) {
-		return false;
+		try{
+			SOAPEnvelope env = smc.getMessage().getSOAPPart().getEnvelope();
+			SOAPHeader sh = getHeader(env);
+			Name name = env.createName("time", "nsTIME", "uri:time");
+			Iterator it = sh.getChildElements(name);
+
+			//check header element
+			if(!it.hasNext()){
+				System.out.println("Time Handler: element not found!");
+				return false;
+			}
+			SOAPElement el = (SOAPElement) it.next();
+			String value = el.getValue();
+			Date dateValue = dateFormatter.parse(value);
+
+			Date dateNow = new Date();
+			long difference = dateNow.getTime() - dateValue.getTime();
+
+			if(difference > 10*1000){
+				System.out.println("Time rejection: interval superior to 10 seconds!");
+				return false;
+			}
+
+			// Put header in a property context
+			String CONTEXT_PROPERTY = "time.property";
+			smc.put(CONTEXT_PROPERTY, value);
+			// Set property scope to application client/server class can access it
+			smc.setScope(CONTEXT_PROPERTY, MessageContext.Scope.APPLICATION);
+
+		} catch (SOAPException | ParseException e) {
+			e.printStackTrace();
+		}
+		return true;
 	}
 
 	private boolean handleOutbound(SOAPMessageContext smc) {
-		return false;
+		try{
+			SOAPEnvelope env = smc.getMessage().getSOAPPart().getEnvelope();
+			SOAPHeader sh = getHeader(env);
+			Name name = env.createName("time", "nsTIME", "uri:time");
+			SOAPHeaderElement el = sh.addHeaderElement(name);
+			el.addTextNode(dateFormatter.format(new Date()));
+		} catch (SOAPException e) {
+			e.printStackTrace();
+		}
+		return true;
 	}
 
 	@Override
